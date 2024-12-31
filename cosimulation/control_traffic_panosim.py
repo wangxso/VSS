edce60d44bd2d1ff3dc11201fec3c08b54b728c7
lynsws <sjj20s@163.com>
2024-12-31 11:12:47 +0800

    [feat] 读取交通车控制命令

--- cosimulation/control_traffic_panosim.py
+++ cosimulation/control_traffic_panosim.py
@@ -4,11 +4,42 @@ from TrafficModelInterface import *
 import math
 import queue
 from loguru import logger
+import json
 import traffic_control
+import mmap
+
 
 calibration_file = r'../Agent/cosimulation/calibration_table.txt'
 control = traffic_control.trafficControl(calibration_file)
 task_queue = queue.Queue()
+
+def receiver():
+    # 文件名和信号量
+    filename = r"C:\PanoSimDatabase\Plugin\channel"
+    
+    # 打开并映射文件
+    with open(filename, 'r+b') as f:
+        # 映射整个文件到内存
+        mm = mmap.mmap(f.fileno(), 1024)
+        try:
+            # 读取消息
+            msg = mm[:].decode('utf-8').strip()
+            if msg:
+                print(f"Received message: {msg}")
+                data = json.loads(msg)
+                command = data.get('command')
+                throttle = data.get('throttle')
+                brake = data.get('brake')
+                steer = data.get('steer')
+                return command,throttle,brake,steer
+            else:
+                print("No message received.")
+                return None
+        finally:
+                # 关闭内存映射
+                mm.close()
+
+
 # 仿真实验启动时回调
 def ModelStart(userData):
     # 初始化待控制交通车信息
@@ -35,29 +66,28 @@ def ModelOutput(userData):
         userData['rebuild'] =1
     # 1. 主车控制信号发送
     try:
+
         # 从队列中取出元组
-        command, throttle, brake, steer = task_queue.get(timeout=1)  # 设置timeout避免无限阻塞
+        command, throttle, brake, steer = receiver()  # 设置timeout避免无限阻塞
         logger.info(f"Command: {command}, Throttle: {throttle}, Brake: {brake}, Steer: {steer}")
-        
-        # 标记任务完成
-        task_queue.task_done()
+        if command == 'traffic_control':
+            # 生成动作指令
+            target_speed, duration, direction = control.control_to_action(userData['traffic_speed'], throttle , brake, steer)
+            # 速度控制
+            changeSpeed(userData['traffic_id'], target_speed, duration)
+            # 方向控制
+            changeLane(userData['traffic_id'], change_lane_direction[direction], duration)
+            # 车辆路口场景判断(待测试通过路口情况)
+            if getRoute(userData['traffic_id']) == next_junction_direction.unknown:
+                valid_route = getValidDirections(getVehicleLane(userData['traffic_id']))
+                if valid_route:
+                        changeRoute(userData['traffic_id'], Direction2Route[direction])
+            # 标记任务完成
+            task_queue.task_done()
     except queue.Empty:
         logger.info("Queue is empty, no items to get.")
 
-    if command == 'traffic_control':
-
-        
-        # 生成动作指令
-        target_speed, duration, direction = control.control_to_action(userData['traffic_speed'], throttle , brake, steer)
-        # 速度控制
-        changeSpeed(userData['traffic_id'], target_speed, duration)
-        # 方向控制
-        changeLane(userData['traffic_id'], change_lane_direction[direction], duration)
-        # 车辆路口场景判断(待测试通过路口情况)
-        if getRoute(userData['traffic_id']) == next_junction_direction.unknown:
-            valid_route = getValidDirections(getVehicleLane(userData['traffic_id']))
-            if valid_route:
-                    changeRoute(userData['traffic_id'], Direction2Route[direction])
+    
 
 # 仿真实验结束时回调
 def ModelTerminate(userData):
